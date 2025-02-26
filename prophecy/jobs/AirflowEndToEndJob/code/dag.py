ed54acb61bdb440aa102b98d57a0b9e27ebd3158
best.neil@prophecy.io <best.neil@prophecy.io>
2025-02-26 13:24:47 +0000

    Upgrade ProphecyLibsPython

--- prophecy/jobs/AirflowEndToEndJob/code/dag.py
+++ prophecy/jobs/AirflowEndToEndJob/code/dag.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 import airflow
 from airflow import DAG
 from airflow.models.param import Param
+from airflow.decorators import task
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from ew9lz_eqkbzridtkugqjfg_.tasks import gold_sales, gold_top_customers, raw_bronze, silver_customers, silver_zip_codes
 PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
@@ -17,11 +18,11 @@ with DAG(
     end_date = pendulum.datetime(2023, 11, 29, tz = "UTC"), 
     catchup = True
 ) as dag:
-    silver_customers_op = silver_customers()
     raw_bronze_op = raw_bronze()
-    gold_top_customers_op = gold_top_customers()
     silver_zip_codes_op = silver_zip_codes()
+    silver_customers_op = silver_customers()
     gold_sales_op = gold_sales()
+    gold_top_customers_op = gold_top_customers()
     raw_bronze_op >> silver_zip_codes_op
     silver_zip_codes_op >> silver_customers_op
     silver_customers_op >> gold_sales_op
